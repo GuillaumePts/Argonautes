1c55e25eb473760556d9b0a4d632ce28966ccef5
Guillaume Pitois <guillaume.pitois1@gmail.com>
2023-02-02 20:45:05 +0100

    suppression gitignore

---
+++ env.js
@@ -0,0 +1,4 @@
+exports.mongoAtlasUri =
+"mongodb+srv://GuillaumePts:dPaOXtnqtCxLfZzU@tchat.sqx0ygb.mongodb.net/Argonautes?retryWrites=true&w=majority";
+
+//  ceci est la string qui permet de se connecter à la BDD
